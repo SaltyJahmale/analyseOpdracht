974941e78a24d838728782dccf6949814094e629
SaltyJahmale <theebag77@gmail.com>
2014-12-06 12:33:17 +0100

    Jenkins test

---
+++ TestJenkins/src/main/java/com/mycompany/testjenkins/NewMain.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testjenkins;
+
+/**
+ *
+ * @author UR mama
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Max max = new Max();
+        System.out.println(max);
+    }
+    
+}
